64211a717af7bd36ae0b03c1b8245fb31be29785
Milan Lesichkov <lesichkovm@gmail.com>
2019-09-21 06:29:53 +0100

    Create run_backup.sh

---
+++ run_backup.sh
@@ -0,0 +1,1 @@
+./imapgrab.py --ssl --server=SERVER --username=YOURUSERNAME --password=PASSWORD --folder=BACKUPDIRPATH --all --download --verbose --maildir -s imap.mail.server --mailboxes="_ALL_" --debug
